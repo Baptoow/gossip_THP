9e79f526500e302784d255756d77772f728a6c90
Baptoow <baptistebarbera@gmail.com>
2020-10-30 00:15:02 +0100

    push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  attr_accessor :password, :password_confirmation
   def show
     @user = User.find(params[:id])
   end
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     puts params[:password]
-   @user = User.new(age: params[:age],city_id: params[:city],first_name: params[:first_name],last_name: params[:secondname],password: params[:password],password_confirmation: params[:passwordverif],description: params[:bio],email: params[:email] )
+    @user = User.new(age: params[:age],city_id: params[:city],first_name: params[:firstname],last_name: params[:secondname],password: params[:password],password_confirmation: params[:passwordverif],description: params[:bio],email: params[:email] )
    if @user.save
      redirect_to "/gossips"
    else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_161039) do
+ActiveRecord::Schema.define(version: 2020_10_29_194314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_161039) do
     t.bigint "city_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["city_id"], name: "index_users_on_city_id"
   end
 
